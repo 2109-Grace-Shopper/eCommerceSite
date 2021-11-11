60493abba89f82b9579cdb18db2bd0dc449b7ef9
evroble <emyvroble@gmail.com>
2021-11-10 19:41:50 -0500

    added route for single product

--- server/api/products.js
+++ server/api/products.js
@@ -3,3 +3,19 @@ const {
   models: { Product },
 } = require('../db');
 module.exports = router;
+
+router.get('/:id', async (req, res, next) => {
+  try{
+    const id = req.params.id;
+
+    const item = await Product.findByPk(id)
+    if (item === null){
+      res.send("I don't seem to have that item right now. Try next season! - Pierre")
+    } else {
+      res.json(item)
+    }
+  } catch(error){
+    next(error)
+  }
+  
+})
