e94f2046e745e1d221c09ae6cc5bd05e52c2be35
Anshu Patel <anshupatel314@gmail.com>
2021-11-11 16:07:04 -0500

    added filtering by category for all products

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,36 +2,86 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchProducts } from '../store/allProducts';
-import { Card, Grid, Button } from '@mui/material';
+import {
+  Card,
+  Grid,
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+} from '@mui/material';
 
 class AllProducts extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: 'all',
+    };
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.filterProducts = this.filterProducts.bind(this);
+  }
+
+  handleFilterChange(evt) {
+    this.setState({ filter: evt.target.value });
+  }
+
+  filterProducts() {
+    let products = this.props.products;
+    if (this.state.filter === 'all') return products;
+    else {
+      return products.filter(
+        (product) => product.category === this.state.filter
+      );
+    }
+  }
+
   componentDidMount() {
     this.props.fetchProducts();
   }
 
   render() {
+    const products = this.filterProducts();
     return (
-      <div className="productsContainer">
-        {this.props.products.length === 0 && <h1>No products to display!</h1>}
-        {this.props.products.map((product) => (
-          <Card key={product.id} sx={{ width: 275, margin: 3 }}>
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Link to="#">
-                <h3 style={{ fontSize: 23 }}>{product.name}</h3>
-              </Link>
-              <img src={product.imageUrl} width="75px" height="75px" />
-              <h3>Price: {product.price}g</h3>
-              <Button variant="contained" sx={{ margin: 2 }}>
-                Add to Cart
-              </Button>
-            </Grid>
-          </Card>
-        ))}
+      <div>
+        <div className="filter">
+          <h3>Filter Products:</h3>
+          <Select
+            id="productsFilter"
+            value={this.state.filter}
+            onChange={this.handleFilterChange}
+            variant="standard"
+            sx={{ m: 1, minWidth: 120 }}
+          >
+            <MenuItem value={'all'}>All</MenuItem>
+            <MenuItem value={'seeds'}>Seeds</MenuItem>
+            <MenuItem value={'trees'}>Trees</MenuItem>
+            <MenuItem value={'stock items'}>Stock Items</MenuItem>
+            <MenuItem value={'farming utility'}>Farming Utility</MenuItem>
+            <MenuItem value={'seasonal decor'}>Seasonal Decor</MenuItem>
+          </Select>
+        </div>
+        <div className="productsContainer">
+          {products.length === 0 && <h1>No products to display!</h1>}
+          {products.map((product) => (
+            <Card key={product.id} sx={{ width: 275, margin: 3 }}>
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Link to="#">
+                  <h3 style={{ fontSize: 23 }}>{product.name}</h3>
+                </Link>
+                <img src={product.imageUrl} width="75px" height="75px" />
+                <h3>Price: {product.price}g</h3>
+                <Button variant="contained" sx={{ margin: 2 }}>
+                  Add to Cart
+                </Button>
+              </Grid>
+            </Card>
+          ))}
+        </div>
       </div>
     );
   }
