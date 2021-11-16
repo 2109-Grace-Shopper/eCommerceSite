273210b87dc716b7d0e3335d2714020f33d978b9
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-11-16 11:57:24 -0500

    added payment

--- client/components/CheckoutPage.js
+++ client/components/CheckoutPage.js
@@ -1,32 +1,42 @@
 import { Box, Button, Typography, FormControl } from '@mui/material';
 import React from 'react';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-
 import AddressForm from "./AddressForm";
 
-const CheckoutPage = () => {
-  return (
-    <Box
-        className="shiping_box"
-        bgcolor="#E4FBFF"
-        justifyContent="center"
-        alignItems="center"
-        sx={{ width: 1000 }}>
-        <div className="checkout_page">
-            <h1>Shipping Address</h1>
-            <AddressForm/>
-            <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                endIcon={<ArrowRightAltIcon />}
-                size="large"
-                >
-                Continue
-            </Button>
-        </div>
-    </Box>
-  );
-};
+class CheckoutPage extends React.Component {
+  constructor(){
+    super();
+  }
+
+  handleSubmit(){
+    
+  }
+
+  render(){
+    return (
+      <Box
+          className="shiping_box"
+          bgcolor="#E4FBFF"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ width: 1000 }}>
+          <div className="checkout_page">
+              <h1>Shipping Address</h1>
+              <AddressForm/>
+              <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  endIcon={<ArrowRightAltIcon />}
+                  size="large"
+                  onClick={()=>this.handleSubmit}
+                  >
+                  Confirm Order
+              </Button>
+          </div>
+      </Box>
+    );
+  };
+}
 
 export default CheckoutPage;
